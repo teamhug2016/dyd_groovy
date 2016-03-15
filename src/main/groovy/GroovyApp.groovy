794faeadc52c6a0b871d4d9262aa9b3a4d81195e
John Fulton <jfulton@thoughtworks.com>
2016-03-15 21:30:18 +1100

    Remove extra files and line in groovyapp

--- src/main/groovy/GroovyApp.groovy
+++ src/main/groovy/GroovyApp.groovy
@@ -2,7 +2,6 @@ import static spark.Spark.*;
 
 public class GroovyApp {
     public static void main(String[] args) {
-	ipAddress("0.0.0.0")
         get("/hello", { req, res -> "Hello World"} )
     }
 }
